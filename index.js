3f79ee6dc2c6eb49578d988ac85fababf4e44b2e
MrEleffant <90548369+MrEleffant@users.noreply.github.com>
2023-08-08 20:56:41 +0200

    Update for demon slayer

--- index.js
+++ index.js
@@ -6,89 +6,98 @@ const puppeteer = require('puppeteer');
     headless: true,
   });
 
-  let from = 388
-  let maxChap = 388
-  const url = 'https://www.scan-vf.net/one_piece/chapitre-'
-  // const url = 'https://www.scan-fr.cc/manga/one-piece/'
-  // const url = 'https://www.scan-vf.net/one_piece/'
+  let fromChap = 51
+  let toChap = 100
+  const manga = 'kimetsu-no-yaiba'
 
-  for (let chap = from; chap < maxChap+1; chap++) {
-    let fullURL = url+chap+'/1'
-    let fullUrlPage2 = url+chap+'/3'
+  const url = 'https://www.scan-vf.net/' + manga + '/chapitre-'
 
-    // console.log(fullUrlPage2)
-    const page = await browser.newPage();
-    
-    await page.goto(fullUrlPage2, {
-      waitUntil: 'networkidle2',
-    });
-    await wait(1000)
-    const elem = await page.$(".img-responsive.scan-page");
-    const boundingBox = await elem.boundingBox();
-    // console.log('boundingBox', boundingBox)
-    
-    await page.goto(fullURL, {
-      waitUntil: 'networkidle2',
-    });
+  for (let chap = fromChap; chap < toChap + 1; chap++) {
+    let fullURL = url + chap + '/1'
+    try {
+      // console.log(fullURL)
+      let fullUrlPage2 = url + chap + '/4'
+      // console.log(fullUrlPage2)
 
-    
-    await wait(2000);
-    
-    const linkHandlers = await page.$x("//a[contains(text(), 'Mode de lecture')]");
-    if (linkHandlers.length > 0) {
-      await linkHandlers[0].click();
-    } else {
-      console.log(fullURL)
-      throw new Error("Mode de Lecture non trouvé " + fullURL);
-    }
-    
-    const fullPage = await page.$x("//a[contains(text(), 'Tout dans une page')]");
-    if (fullPage.length > 0) {
-      await fullPage[0].click();
-    } else {
-      console.log(fullURL)
-      throw new Error("Tout dans une page non trouvé" + fullURL);
-    }
+      const page = await browser.newPage();
 
-    await autoScroll(page);
-    
-    setTimeout(async () => {
-      await page.pdf({ path: 'PDFS/one-piece-'+chap+'.pdf', height: (1.1*boundingBox.height)+'px',width: (1.1*boundingBox.width)+'px', displayHeaderFooter: false, printBackground: false});
-      
-      // await page.screenshot({
-      //   path: 'PDFS/one-piece-'+chap+'.png',
-      //   fullPage: true
-      // });
-      
-      console.log(`Chapitre ${chap} téléchargé - ${boundingBox.height} x ${boundingBox.width}`)
+      await page.goto(fullUrlPage2, {
+        waitUntil: 'networkidle2',
+      });
+      await wait(1000)
+      const elem = await page.$(".img-responsive.scan-page");
+      const boundingBox = await elem.boundingBox();
+      // console.log('boundingBox', boundingBox)
+
+      await page.goto(fullURL, {
+        waitUntil: 'networkidle2',
+      });
+
+
       await wait(2000);
-      await page.close(); 
-      
-    }, 2000)
+
+      const linkHandlers = await page.$x("//a[contains(text(), 'Mode de lecture')]");
+      if (linkHandlers.length > 0) {
+        await linkHandlers[0].click();
+      } else {
+        console.log(fullURL)
+        throw new Error("Mode de Lecture non trouvé " + fullURL);
+      }
+
+      const fullPage = await page.$x("//a[contains(text(), 'Tout dans une page')]");
+      if (fullPage.length > 0) {
+        await fullPage[0].click();
+      } else {
+        console.log(fullURL)
+        throw new Error("Tout dans une page non trouvé" + fullURL);
+      }
+
+      await autoScroll(page);
+
+      setTimeout(async () => {
+        await page.pdf({ path: 'PDFS/' + manga + '-' + chap + '.pdf', height: (1.1 * boundingBox.height) + 'px', width: (1.1 * boundingBox.width) + 'px', displayHeaderFooter: false, printBackground: false });
+
+        // await page.screenshot({
+        //   path: 'PDFS/one-piece-'+chap+'.png',
+        //   fullPage: true
+        // });
+
+        console.log(`Chapitre ${chap} téléchargé - ${boundingBox.height} x ${boundingBox.width}`)
+        await wait(2000);
+        await page.close();
+
+      }, 2000)
+
+    } catch (error) {
+      console.log(fullURL)
+      console.log(error)
+    }
+
+
   }
 })();
 
-async function wait(ms){
+async function wait(ms) {
   await setTimeout(async () => {
     return
   }, ms);
 }
 
-async function autoScroll(page){
+async function autoScroll(page) {
   await page.evaluate(async () => {
-      await new Promise((resolve, reject) => {
-          var totalHeight = 0;
-          var distance = 100;
-          var timer = setInterval(() => {
-              var scrollHeight = document.body.scrollHeight;
-              window.scrollBy(0, distance);
-              totalHeight += distance;
+    await new Promise((resolve, reject) => {
+      var totalHeight = 0;
+      var distance = 100;
+      var timer = setInterval(() => {
+        var scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
 
-              if(totalHeight >= scrollHeight){
-                  clearInterval(timer);
-                  resolve();
-              }
-          }, 50);
-      });
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 50);
+    });
   });
 }
